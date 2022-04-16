47a316ded9cad4adbf6537a9b27062392153661b
= <=>
2022-04-16 19:30:37 -0300

    Finaliza exercicio

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿double number ;
+
+Console.Write("Digite um número:");
+number = Convert.ToDouble (Console.ReadLine());
+
+if (0 > number)
+{
+    Console.WriteLine("Você digitou um número negativo.");
+}
